1c79aa5fbf25863433c7a4e9cc73a958f836eb29
ananyapandey10 <ananya.tech07@gmail.com>
2024-09-28 18:41:52 +0530

    initial message

---
+++ index.js
@@ -0,0 +1,44 @@
+//https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Ananya
+
+function generateQR(){
+    let imgQR = document.getElementById("qrImage");
+    let qrText = document.getElementById("qrText");
+    
+   imgQR.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=I www.linkedin.com" ;
+
+}
+
+
+
+
+//www.linkedin.com/in/ananya-pandey-772105261
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
